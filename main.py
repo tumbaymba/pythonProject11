bd719b4aa9194c08f3953854179818c0e86807a0
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-03-25 19:37:17 +0300

    Add calling methods

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from database.db_manager import DBManager
 from database.hh_vacansies import HH_api_db
 from config import config
 from database.utils import create_database, create_table
@@ -14,3 +15,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    print(DBManager.get_vacancies_with_keyword('python'))
+    print(DBManager.get_vacancies_with_higher_salary())
+    print(DBManager.get_all_vacancies())
+    print(DBManager.get_avg_salary())
+    print(DBManager.get_companies_and_vacancies_count())
+
